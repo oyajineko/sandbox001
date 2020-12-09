8674355d9d691115a8f6a289b1814e5114213fdd
oyajineko <75714902+oyajineko@users.noreply.github.com>
2020-12-09 15:02:05 +0900

    初回です 色々勉強中

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import "./styles.css";
+/**
+ * const, let
+//  */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+
+// var val1 = "変数を再宣言";
+// console.log(val1);
+// const name = "おやじ猫";
+// const age = 28;
+// const message1 = `私の名前は${name}です。`;
+
+// console.log(message1);
+
+// const func2 = (str) => {
+//   return str;
+// };
+
+// console.log(func2("おやじ猫参上！"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(4, 5));
+
+const myProfile = {
+  name: "おやじ猫",
+  favorit: "neko",
+  age: 49
+};
+
+const { favorit, age } = myProfile;
+
+const message2 = `名前は${favorit}です。年齢は${age}です。`;
+console.log(message2);
